24d05ba0240fd5e649ffe8b8ee104fa85c24a751
zero <osmk@qq.com>
2015-06-06 17:47:59 +0800

    Initial Commit

---
+++ GGSS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GGSS
+//
+//  Created by __无邪_ on 15/6/6.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
